28b48f8d54d1346bb2d205fa9453ce73e82314ab
Dennis Hulme <dhbcarvalho@yahoo.com.br>
2022-03-18 12:12:48 -0300

    codigo que funciona no localhost

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Markdown Previewer
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/dhbcarvalho/pen/OJOOqjg](https://codepen.io/dhbcarvalho/pen/OJOOqjg).
+
+
---
+++ dist/script.js
@@ -0,0 +1,160 @@
+/* globals marked, Prism, React, ReactDOM */
+/* eslint-disable react/prop-types, no-nested-ternary */
+
+// View a more complex version of this project with custom toolbar here:
+// https://codepen.io/no_stack_dub_sack/pen/JbPZvm?editors=0110
+
+// coded by @no-stack-dub-sack (github) / @no_stack_sub_sack (Codepen)
+
+// eslint-disable-next-line no-unused-vars
+const projectName = "markdown-previewer";
+
+// ALLOWS LINE BREAKS WITH RETURN BUTTON
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, "javascript");
+  } });
+
+
+// INSERTS target="_blank" INTO HREF TAGS (required for Codepen links)
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+      editorMaximized: false,
+      previewMaximized: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value });
+
+  }
+  handleEditorMaximize() {
+    this.setState({
+      editorMaximized: !this.state.editorMaximized });
+
+  }
+  handlePreviewMaximize() {
+    this.setState({
+      previewMaximized: !this.state.previewMaximized });
+
+  }
+  render() {
+    const classes = this.state.editorMaximized ?
+    ["editorWrap maximized", "previewWrap hide", "fa fa-compress"] :
+    this.state.previewMaximized ?
+    ["editorWrap hide", "previewWrap maximized", "fa fa-compress"] :
+    ["editorWrap", "previewWrap", "fa fa-arrows-alt"];
+    return /*#__PURE__*/(
+      React.createElement("div", null, /*#__PURE__*/
+      React.createElement("div", { className: classes[0] }, /*#__PURE__*/
+      React.createElement(Toolbar, {
+        icon: classes[2],
+        onClick: this.handleEditorMaximize,
+        text: "Editor" }), /*#__PURE__*/
+
+      React.createElement(Editor, { markdown: this.state.markdown, onChange: this.handleChange })), /*#__PURE__*/
+
+      React.createElement("div", { className: "converter" }), /*#__PURE__*/
+      React.createElement("div", { className: classes[1] }, /*#__PURE__*/
+      React.createElement(Toolbar, {
+        icon: classes[2],
+        onClick: this.handlePreviewMaximize,
+        text: "Previewer" }), /*#__PURE__*/
+
+      React.createElement(Preview, { markdown: this.state.markdown }))));
+
+
+
+  }}
+
+
+const Toolbar = props => {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "toolbar" }, /*#__PURE__*/
+    React.createElement("i", { className: "fa fa-free-code-camp", title: "no-stack-dub-sack" }),
+    props.text, /*#__PURE__*/
+    React.createElement("i", { className: props.icon, onClick: props.onClick })));
+
+
+};
+
+const Editor = props => {
+  return /*#__PURE__*/(
+    React.createElement("textarea", {
+      id: "editor",
+      onChange: props.onChange,
+      type: "text",
+      value: props.markdown }));
+
+
+};
+
+const Preview = props => {
+  return /*#__PURE__*/(
+    React.createElement("div", {
+      dangerouslySetInnerHTML: {
+        __html: marked(props.markdown, { renderer: renderer }) },
+
+      id: "preview" }));
+
+
+};
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![abcd Logo](https://st2.depositphotos.com/8865038/48213/i/1600/depositphotos_482136564-stock-photo-inscription-acme-written-black-paint.jpg)
+`;
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("app"));
